416fb0d823a12c43a3c21d68fd1ff10220546b34
root <kisflying@gmail.com>
2015-07-21 11:37:11 +0800

    Update the noejs.

--- app.js
+++ app.js
@@ -1,36 +1,13 @@
+var express = require('express');
+var app = express();
 
-/**
- * Module dependencies.
- */
-
-var express = require('express')
-  , routes = require('./routes');
-
-var app = module.exports = express.createServer();
-
-// Configuration
-
-app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+app.get('/', function (req, res) {
+  res.send('Hello World!');
 });
 
-app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
 
-app.configure('production', function(){
-  app.use(express.errorHandler());
-});
-
-// Routes
-
-app.get('/', routes.index);
-
-app.listen(3000, function(){
-  console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
+  console.log('Example app listening at http://%s:%s', host, port);
 });
