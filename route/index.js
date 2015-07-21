857282f07b5841c5b4e326abd6d3a458b030467a
Kris Neal <kisflying@gmail.com>
2015-07-21 14:48:50 +0800

    Update

--- route/index.js
+++ route/index.js
@@ -2,13 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-	var t = {id:1, name:'Kris'};
-	
+	var t = {id:1, name:'Get'};
+
 	res.send(t);
 });
 
 router.post('/', function(req, res, next) {
-	var t = {id:1, name:'Post'};
+	var t = {time:new Time(), name:'Post'};
+
+	res.send(t);
+});
+
+router.put('/', function(req, res, next) {
+	var t = {time:new Time(), name:'Put'};
 
 	res.send(t);
 });
