5a2e465c59a5b2a34bd75c0a858144df5b25c7cf
root <kisflying@gmail.com>
2015-07-21 15:33:59 +0800

    Update the index.js.

--- route/index.js
+++ route/index.js
@@ -2,19 +2,25 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-	var t = {id:1, name:'Get'};
+	var seq = 0;
+
+	if(req.query.id){
+		seq = req.query.id;
+	}
+
+	var t = {id:1, name:'Get', seq: seq, ip:req.ip};
 
 	res.send(t);
 });
 
 router.post('/', function(req, res, next) {
-	var t = {time:new Time(), name:'Post'};
+	var t = {time:new Date(), name:'Post'};
 
 	res.send(t);
 });
 
 router.put('/', function(req, res, next) {
-	var t = {time:new Time(), name:'Put'};
+	var t = {time:new Date(), name:'Put'};
 
 	res.send(t);
 });
