511c0a72020146a2fdf80e2f07ec6a0da660cac4
root <kisflying@gmail.com>
2015-07-21 11:46:17 +0800

    Add the pg library.

--- app.js
+++ app.js
@@ -1,7 +1,26 @@
 var express = require('express');
 var app = express();
+app.use(express.static('public'));
 
 app.get('/', function (req, res) {
+var pg = require('pg');
+var conString = "postgres://username:password@localhost/database";
+
+pg.connect(conString, function(err, client, done) {
+
+  if (err) {
+    return console.error('error fetching client from pool', err);
+  }
+  client.query('SELECT $1::int AS number', ['1'], function(err, result) {
+    done();
+    if (err) {
+      return console.error('error running query', err);
+    }
+    console.log(result.rows[0].number);
+  });
+
+});
+
   res.send('Hello World!');
 });
 
