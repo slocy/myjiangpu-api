467e36681e5ac770cbe1782c934e89974e442da4
Kris Neal <kisflying@gmail.com>
2015-07-22 17:00:17 +0800

    Update the data model.

--- db_model.js
+++ db_model.js
@@ -5,11 +5,83 @@ function configurations(){
 
 function customer(){
 	this.wechatId = ''; // Get it from Wechat API.
-	this.Name = '';
-	this.NickName = '';
-	this.Gender = 'Male';
-	this.Email = '';
-	this.Cellphone = '';
+	this.name = '';
+	this.nickname = '';
+	this.gender = 'Male';
+	this.email = '';
+	this.cellphone = '';
 }
 
+function artisan(){
+	this.artisanId = '';
+	this.name = '';
+	this.wechatId = '';
+	this.cellphone = '';
+	this.description = '';
+	this.city = '';
+	this.geo = '';
+}
 
+function book(){
+	this.bookId = '';
+	this.title = '';
+	this.subTitle = '';
+	this.description = '';
+	this.primaryImage = '';
+	this.primaryVideo = '';
+	this.images = [];
+}
+
+function bookSteps(){
+	this.stepId = '';
+	this.name = '';
+	this.content = '';
+	this.images = [];
+	this.videos = [];
+}
+
+function bookStuffs(){
+	this.stuffId = '';
+	this.title = '';
+	this.description = '';
+	this.price = 0;
+	this.images = [];
+}
+
+function utility(){
+	this.utilityId = '';
+	this.name = '';
+	this.description = '';
+	this.images = [];
+	this.price = 0;
+}
+
+function lesson(){
+	this.lessonId = '';
+	this.title = '';
+	this.customerIds = [];
+	this.artisanId = '';
+	this.bookId = '';
+	this.datetime = '';
+	this.place = '';
+	this.price = 0;
+}
+
+function order(){
+	this.orderId = '';
+	this.type = ''; // can be utility, lesson, stuff...
+	this.quantity = 1;
+	this.summary = 0;
+	this.paymentId = '';
+	this.customerId = '';
+	this.lessonId = '';
+	this.utilityId = '';
+	this.stuffId = '';
+}
+
+function payment(){
+	this.paymentId = '';
+	this.summary = 0;
+	this.isPayed = 0;
+	this.transaction = '';
+}
