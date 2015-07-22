c8bedb85645bb434211e4e4c704132f46ba840b3
Kris Neal <kisflying@gmail.com>
2015-07-22 23:21:26 +0800

    Update the atry.

--- route/atry.js
+++ route/atry.js
@@ -2,6 +2,14 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 
+
+var initUsersSchema = mongoose.Schema({
+    userid: Number,
+    username: String,
+    auth_path: String,
+    auth_date: String
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	mongoose.connect('mongodb://localhost/pilotdatabase');
@@ -11,6 +19,12 @@ router.get('/', function(req, res, next) {
 	db.once('open', function (callback) {
 	  console.log('opened');
 	});
+
+	var init = mongoose.model('init', initUsersSchema);
+	var i = new init({ userid: 3, username: 'mongoose', auth_path:'123', auth_date: '2015-01-01' });
+	console.log(i.username); 
+
+	Kitten.find({ name: /^Fluff/ }, callback);
 
 	var t = {id:1, name:'Kris'}
 	res.send(t);
