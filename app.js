a4f118bdca957d7e2a704a692cdc946da6565cff
Kris Neal <kisflying@gmail.com>
2015-07-22 21:18:39 +0800

    Add the favicon.ico

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 
 app.use('/', index);
 app.use('/sharing', sharing);
