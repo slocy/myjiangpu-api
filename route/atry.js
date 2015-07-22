9a87b1cc9baad0a82dcd29daf22b8a40f157aa48
Kris Neal <kisflying@gmail.com>
2015-07-22 22:42:26 +0800

    Add the mongodb atry.js

--- route/atry.js
+++ route/atry.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+	mongoose.connect('mongodb://localhost/pilotdatabase');
+
+	var db = mongoose.connection;
+	db.on('error', console.error.bind(console, 'connection error:'));
+	db.once('open', function (callback) {
+	  console.log('opened');
+	});
+
 	var t = {id:1, name:'Kris'}
 	res.send(t);
 });
