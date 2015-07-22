4a4bc10903ff02ed8106357a36f0018ba64f5a7e
root <kisflying@gmail.com>
2015-07-22 16:03:06 +0800

    Update the model.js

--- db_model.js
+++ db_model.js
@@ -1,2 +1,15 @@
 // This file shows all mongodb data schemas which can be used in nodejs application.
+function configurations(){
+	
+}
+
+function customer(){
+	this.wechatId = ''; // Get it from Wechat API.
+	this.Name = '';
+	this.NickName = '';
+	this.Gender = 'Male';
+	this.Email = '';
+	this.Cellphone = '';
+}
+
 
