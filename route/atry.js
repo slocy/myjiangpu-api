bd18c8726e75267259e2663c0f7274f7fb403ed9
root <kisflying@gmail.com>
2015-07-23 16:56:29 +0800

    Update

--- route/atry.js
+++ route/atry.js
@@ -5,12 +5,18 @@ var mgClient = mongodb.MongoClient;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	mgClient.connect('mongodb://localhost:27017/pilotdatabase', function(err,db){
+	mgClient.connect('mongodb://localhost:27017/PilotDatabase', function(err,db){
 		if(err) return console.dir(err);
 
 		var init = db.collection('InitUsers');
 
-		res.send(init.findOne());
+		init.find({userid : 2}, function(err, item){
+			console.log(item);
+		});
+
+		db.close();
+		
+		return res.send(result);
 	});
 });
 
