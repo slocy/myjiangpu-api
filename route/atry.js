bd9df8c60f93cb918fef5b484d4aa53d6ab9e608
Kris Neal <kisflying@gmail.com>
2015-07-23 16:36:34 +0800

    Update

--- route/atry.js
+++ route/atry.js
@@ -1,33 +1,22 @@
 var express = require('express');
 var router = express.Router();
-var mongoose = require('mongoose');
-
-
-var initUsersSchema = mongoose.Schema({
-    userid: Number,
-    username: String,
-    auth_path: String,
-    auth_date: String
-});
+var mongodb = require('mongodb');
+var mgClient = mongodb.MongoClient;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	mongoose.connect('mongodb://localhost/pilotdatabase');
+	var result = null;
 
-	var db = mongoose.connection;
-	db.on('error', console.error.bind(console, 'connection error:'));
-	db.once('open', function (callback) {
-	  console.log('opened');
-	});
+	mgClient.connect('mongodb://localhost:27017/pilotdatabase', function(err,db){
+		if(err) return console.dir(err);
 
-	var init = mongoose.model('init', initUsersSchema);
-	//var i = new init({ userid: 3, username: 'mongoose', auth_path:'123', auth_date: '2015-01-01' });
-	//console.log(i.username); 
+		var init = db.collection('InitUsers');
 
-	//var result = init.find();
+		result = init.find()
+	})
 
-	var t = {id:1, name:'Kris'};
-	res.send(t);
+	//var t = {id:1, name:'Kris'};
+	res.send(result);
 });
 
 module.exports = router;
